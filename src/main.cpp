e5e147494e29a4a3cde4bf211a3c3b9030b11225
denta58 <vereslakosszabolcs@gmail.com>
2022-11-21 14:28:47 +0100

    only_pressure

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,11 @@
 #include <Arduino_LSM9DS1.h>
 #include <ArduinoBLE.h>
 
-const int VERSION = 0x00000001;
-const float TEMPERATURE_CALIBRATION = -5.0;
+ #define GREEN 23
+ #define RED 22
 
-float slpALT = 1013.25;
-
-BLEService                     service                 (("181A"));
-BLEUnsignedIntCharacteristic   slpCorCharacteristic    (("0001"), BLEWrite);
-BLEIntCharacteristic        temperatureCharacteristic  (("2A6E"), BLENotify);
-BLEIntCharacteristic        pressureCharacteristic     (("2A6D"), BLENotify);
-BLEIntCharacteristic         humidityCharacteristic    (("2A6F"), BLENotify);
-BLEIntCharacteristic         altitudeCharacteristic    (("2A6C"), BLENotify);
+BLEService                     service                 ("181A");
+BLEIntCharacteristic        pressureCharacteristic     ("2A6D", BLENotify);
 
 //#define DEBUG True
 // String to calculate the local and device name
@@ -29,12 +23,17 @@ void printSerialMsg(const char * msg) {
   #endif
 }
 
-void blinkLoop() {
+void blinkLoop(int led) {
+  
+
   while (1) {
-    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(led, !HIGH);
     delay(500);
-    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(led, !LOW);
     delay(500);
+    if (led == GREEN){
+      break;
+    }
   }
 }
 
@@ -44,23 +43,28 @@ void setup() {
   while (!Serial);
   Serial.println("Started");
   #endif
+  
+  pinMode(GREEN, OUTPUT);
+  pinMode(RED, OUTPUT);
 
-  delay(2000);
+  digitalWrite(GREEN,HIGH);
+  digitalWrite(RED,HIGH);
 
-  if (!HTS.begin()) {
-    printSerialMsg("Failed to initialized HTS!");
-    blinkLoop();
-  }
+  
 
   if (!BARO.begin()) {
     printSerialMsg("Failed to initialized BARO!");
-    blinkLoop();
+    blinkLoop(RED);
   }
-  
+  blinkLoop(GREEN);
+
   if (!BLE.begin()) {
     printSerialMsg("Failed to initialized BLE!");
-    blinkLoop();
+    blinkLoop(RED);
   }
+  blinkLoop(GREEN);
+
+
 
   String address = BLE.address();
   #ifdef DEBUG
@@ -88,69 +92,28 @@ void setup() {
   BLE.setDeviceName(name.c_str());
   BLE.setAdvertisedService(service);
 
-  service.addCharacteristic(altitudeCharacteristic);
+  
   service.addCharacteristic(pressureCharacteristic);
-  service.addCharacteristic(humidityCharacteristic);
-  service.addCharacteristic(temperatureCharacteristic);
-  service.addCharacteristic(slpCorCharacteristic);
-  slpCorCharacteristic.setValue(VERSION);
 
   BLE.addService(service);
   BLE.advertise();
-}
-
-
-float readAltitude(float seaLevel,float pres) {
-  return 44330.0 * (1.0 - pow(pres / seaLevel, 0.1903));
+ 
 }
 
 void updateSubscribedCharacteristics() {
-  bool doTemperature = temperatureCharacteristic.subscribed();
-  bool doHumidity = humidityCharacteristic.subscribed();
-  if (doTemperature || doHumidity) {
-    float temperature = HTS.readTemperature();
-    float temperatureCalibrated = temperature + TEMPERATURE_CALIBRATION;
-    if (doTemperature) {
-      int tTemperature = temperatureCalibrated*100;
-      Serial.print("T: ");
-      Serial.println(tTemperature);
-      temperatureCharacteristic.writeValue(tTemperature);
-    }
-    if (doHumidity) {
-      float humidity = HTS.readHumidity();
-      float dp = temperature - ((100.0 - humidity) / 5.0);
-      float humidityCalibrated = (100.0 - (5.0 * (temperatureCalibrated - dp)));
-      Serial.print("H: ");
-      int hHumidity=humidityCalibrated*100;
-      Serial.println(hHumidity);
-      humidityCharacteristic.writeValue(hHumidity);
-    }
-  }
   if (pressureCharacteristic.subscribed()) {
     float pressure = BARO.readPressure();
     int pPressure=pressure*10000;
     Serial.print("P: ");
     Serial.println(pPressure);
-    pressureCharacteristic.writeValue(pPressure);
+    digitalWrite(GREEN, !digitalRead(GREEN));
+    Serial.println(pressureCharacteristic.writeValue(pPressure));
   }
-  if (slpCorCharacteristic.written()) {
-        int rawSLP = slpCorCharacteristic.value();    // any value other than 0
-        slpALT=rawSLP/100.0F;
-        Serial.println(slpALT);
-  }
-
-   if (altitudeCharacteristic.subscribed()) {
-    float prs = BARO.readPressure()*10.F;
-    Serial.println(prs);
-    float alti= readAltitude(slpALT,prs)*100;
-    Serial.print("A: ");
-    Serial.println(alti);
-    altitudeCharacteristic.writeValue(alti);
-  }
+ 
 }
 
 void loop() {
-  BLE.poll(1000);
+  BLE.poll(100);
   while (BLE.connected()) {
     updateSubscribedCharacteristics();
     delay(500);
